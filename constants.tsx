9ca8ffb2c7faa20317954d6ba54ccc2b6f3cf563
Samfonrojas <samfonrojas@gmail.com>
2025-09-27 21:31:41 -0500

    Update gold icon with new metal bars image

--- constants.tsx
+++ constants.tsx
@@ -7,7 +7,7 @@ export const METALS: Metal[] = [
     symbol: 'Au',
     name: 'Oro',
     description: 'El metal precioso por excelencia, una reserva de valor global y símbolo de riqueza.',
-    icon: <img src="/Gemini_Generated_Image_cjz426cjz426cjz4.png" alt="Gold bars" className="w-12 h-12 object-contain" />,
+    icon: <img src="/Gemini_Generated_Image_tmws7utmws7utmws copy copy.png" alt="Gold bars" className="w-12 h-12 object-contain" />,
   },
   {
     symbol: 'Rh',
